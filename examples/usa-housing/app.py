98e0b7fffd93ff7301282ab5371efd5fd1169e39
Raman Singh <ramandeep.singh@abl-solutions.com>
2022-06-28 08:28:52 +0200

    some changes

--- examples/usa-housing/app.py
+++ examples/usa-housing/app.py
@@ -47,7 +47,7 @@ def main():
     y = housing_data['Price']
 
     test_size=0.3
-    random_state=50
+    random_state=51
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=random_state)
 
